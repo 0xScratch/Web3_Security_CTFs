204edc7cddbaca983bd9782b8384615d55f6f8a0
Aryan Malik <aryanhunny98@gmail.com>
2024-06-27 18:26:56 +0530

    fixed some positioning errors

--- Ethernaut/questions/11.Elevator.sol
+++ Ethernaut/questions/11.Elevator.sol
@@ -13,7 +13,6 @@ interface Building {
   function isLastFloor(uint) external returns (bool);
 }
 
-
 contract Elevator {
   bool public top;
   uint public floor;
@@ -21,7 +20,7 @@ contract Elevator {
   function goTo(uint _floor) public {
     Building building = Building(msg.sender);
 
-    if (! building.isLastFloor(_floor)) {
+    if (!building.isLastFloor(_floor)) {
       floor = _floor;
       top = building.isLastFloor(floor);
     }
